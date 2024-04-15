b562b49ce8963f421dc233db19500e23aa26f8d1
Bennett Ballentine <bennett.ballentine@gmail.com>
2024-04-15 19:22:01 -0400

    handle requests in a go-routine

--- simplehttp.go
+++ simplehttp.go
@@ -17,67 +17,70 @@ func StartServer(port int) {
 	fmt.Println("Listening on port", port)
 
 	for {
-		fmt.Print("Waiting for connection...\n\n")
+		fmt.Print("Waiting for connection...\n")
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatal("Failed to accept incoming connection: ", err)
 		}
 
-		fmt.Println("============= Talking to", conn.RemoteAddr(), "=============")
+		go handleConnection(conn)
+	}
+}
 
-		// read data in chunks of 1kB
-		tmp := make([]byte, 1024)
-		data := make([]byte, 0)
-		packet := packet{}
-		length := 0
+func handleConnection(conn net.Conn) {
+	fmt.Println("============= Talking to", conn.RemoteAddr(), "=============")
 
-		for {
-			// TODO: add timeout here and also for the whole loop
-			n, err := conn.Read(tmp)
-			if err != nil {
-				if err != io.EOF {
-					fmt.Println("Read error -", err)
-				}
-				fmt.Println("here in EOF")
-				break
+	// read data in chunks of 1kB
+	tmp := make([]byte, 1024)
+	data := make([]byte, 0)
+	packet := packet{}
+	length := 0
+
+	for {
+		// TODO: add timeout here and also for the whole loop
+		n, err := conn.Read(tmp)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Read error -", err)
 			}
-
-			data = append(data, tmp[:n]...)
-			length += n
-			clear(tmp)
-
-			// check if we have a full packet yet
-			packet, err = parsePacket(string(data))
-			if err != nil {
-				// TODO: don't continue forever. Set a maximum packet size?
-				continue
-			}
-
+			fmt.Println("here in EOF")
 			break
 		}
 
-		fmt.Print(packet.buildString())
+		data = append(data, tmp[:n]...)
+		length += n
+		clear(tmp)
 
-		// send a response
-		conn.Write([]byte("HTTP/1.0 200 OK\r\n" +
-			"Content-Type: text/html; charset=UTF-8\r\n" +
-			"Content-Length: 237\r\n" +
-			"\r\n" +
-			"<!DOCTYPE html>\r\n" +
-			"<html lang=\"en\">\r\n" +
-			"<head>\r\n" +
-			"  <meta charset=\"UTF-8\" />\r\n" +
-			"  <title>Hello, world!</title>\r\n" +
-			"  <meta name=\"viewport\" content=\"width=device-width,initial-scale=1\" />\r\n" +
-			"</head>\r\n" +
-			"<body>\r\n" +
-			"  <h1>Hello, world!</h1>\r\n" +
-			"</body>\r\n" +
-			"</html>\r\n"))
+		// check if we have a full packet yet
+		packet, err = parsePacket(string(data))
+		if err != nil {
+			// TODO: don't continue forever. Set a maximum packet size?
+			continue
+		}
 
-		conn.Close()
-
-		fmt.Print("\n=====================================================\n\n")
+		break
 	}
 
+	fmt.Print(packet.buildString())
+
+	// send a response
+	conn.Write([]byte("HTTP/1.0 200 OK\r\n" +
+		"Content-Type: text/html; charset=UTF-8\r\n" +
+		"Content-Length: 237\r\n" +
+		"\r\n" +
+		"<!DOCTYPE html>\r\n" +
+		"<html lang=\"en\">\r\n" +
+		"<head>\r\n" +
+		"  <meta charset=\"UTF-8\" />\r\n" +
+		"  <title>Hello, world!</title>\r\n" +
+		"  <meta name=\"viewport\" content=\"width=device-width,initial-scale=1\" />\r\n" +
+		"</head>\r\n" +
+		"<body>\r\n" +
+		"  <h1>Hello, world!</h1>\r\n" +
+		"</body>\r\n" +
+		"</html>\r\n"))
+
+	conn.Close()
+
+	fmt.Print("\n=====================================================\n\n")
 }
